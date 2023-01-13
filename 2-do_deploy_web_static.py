ebe200f6f8e4695c8e54a3c13436a6ddcd072a12
Werku Birhanu Amsal <workzman12@gmail.com>
2023-01-13 17:02:40 +0300

    Flask Project

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -42,7 +42,7 @@ def do_deploy(archive_path):
         return False
     if run("rm -rf /data/web_static/current").failed is True:
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+    if run("ln -sf /data/web_static/releases/{}/ /data/web_static/current".
            format(name)).failed is True:
         return False
     return True
